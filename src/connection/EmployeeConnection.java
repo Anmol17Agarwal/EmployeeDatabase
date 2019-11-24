6daea10e4148ea7f1cfde95bd193a3982193c065
dbc2201 <divyanshb22@gmail.com>
2019-11-25 01:11:32 +0530

    declare the password for the database

--- src/connection/EmployeeConnection.java
+++ src/connection/EmployeeConnection.java
@@ -3,4 +3,5 @@ package connection;
 public class EmployeeConnection {
     public static final String DATABASE_URL = "jdbc:mysql://localhost:3306/test";
     public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
 }
