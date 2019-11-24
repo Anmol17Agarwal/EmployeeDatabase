a8f56cd48e32ffa4c69b98495482dff2b86dc1f1
dbc2201 <divyanshb22@gmail.com>
2019-11-25 01:11:15 +0530

    declare the username for the database

--- src/connection/EmployeeConnection.java
+++ src/connection/EmployeeConnection.java
@@ -2,4 +2,5 @@ package connection;
 
 public class EmployeeConnection {
     public static final String DATABASE_URL = "jdbc:mysql://localhost:3306/test";
+    public static final String USERNAME = "root";
 }
