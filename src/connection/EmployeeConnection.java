2ae37aeca8bad87c662c0d5529e2167a51b28e18
dbc2201 <divyanshb22@gmail.com>
2019-11-25 01:12:11 +0530

    create an object of the Connection class

--- src/connection/EmployeeConnection.java
+++ src/connection/EmployeeConnection.java
@@ -1,7 +1,10 @@
 package connection;
 
+import java.sql.Connection;
+
 public class EmployeeConnection {
     public static final String DATABASE_URL = "jdbc:mysql://localhost:3306/test";
     public static final String USERNAME = "root";
     public static final String PASSWORD = "";
+    private static Connection connection = null;
 }
