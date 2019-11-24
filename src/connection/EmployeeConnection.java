25abd25c1e1f4ccd517bde7cd0c419f2d43ffb4e
dbc2201 <divyanshb22@gmail.com>
2019-11-25 01:55:50 +0530

    change access modifiers

--- src/connection/EmployeeConnection.java
+++ src/connection/EmployeeConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class EmployeeConnection {
-    public static final String DATABASE_URL = "jdbc:mysql://localhost:3306/test";
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/test";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
     private static Connection connection = null;
 
     public static Connection getConnection() {
