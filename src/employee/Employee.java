859afe1f615f694d2144fd1571494ce8db48b068
dbc2201 <divyanshb22@gmail.com>
2019-11-25 00:54:41 +0530

    add getters and setters for attributes of class:Employee

--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -10,4 +10,28 @@ public class Employee {
         this.empName = empName;
         this.empSalary = empSalary;
     }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public double getEmpSalary() {
+        return empSalary;
+    }
+
+    public void setEmpSalary(double empSalary) {
+        this.empSalary = empSalary;
+    }
 }
